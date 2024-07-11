a520194159472f16d4517c3ddc69acb6205261d4
kush sharma <kush.73888@gmail.com>
2024-07-11 20:34:33 +0530

    Basic Footer Setup

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -23,7 +23,7 @@ const Footer = () => {
         </a>
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
-        <p className="md:text-base text-sm md:font-normal font-light">
+        <p className="md:text-base text-sm md:font-normal font-light md:mt-2">
           Copyright © 2024 Kush Sharma
         </p>
 
