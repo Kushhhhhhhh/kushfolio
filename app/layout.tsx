9c1311bbb3032534dc790ac2a5bdcb8a8822a5a2
kush sharma <kush.73888@gmail.com>
2024-09-08 09:36:03 +0530

    Changed the metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "@/components/theme-provider"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Kushfolio",
+  title: "Kush Sharma - Personal Portfolio",
   description: "Modern and Minimalist Portfolio",
 };
 
