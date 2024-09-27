6b21eac1a5b43c6b7597b6dd7491ca33d1d607ec
kush sharma <kush.73888@gmail.com>
2024-09-27 18:38:57 +0530

    Images Updated + Email Updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -16,7 +16,7 @@ const Footer = () => {
           Reach out to me today and let&apos;s discuss how I can help you
           achieve your goals.
         </p>
-        <Link href="mailto:kush.73888@gmail.com">
+        <Link href="mailto:fullstack.kush@gmail.com">
           <MagicButton
             title="Let's get in touch"
             icon={<FaLocationArrow />}
